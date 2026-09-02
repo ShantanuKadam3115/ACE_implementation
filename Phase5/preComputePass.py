7193721f329a7cabe9da622848698d48ebc9eb67
Shantanu Kadam <shantanukadam3115@gmail.com>
2026-09-03 00:53:47 +0530

    phase5 working.

--- Phase5/preComputePass.py
+++ Phase5/preComputePass.py
@@ -21,11 +21,25 @@ flat_indices = torch.randperm(grid_h*grid_w)[:samples_per_frame]
 
 rows, cols = torch.unravel_index(flat_indices, (grid_h, grid_w))
 
-fancy_indexing = img_featured[:512,512]
 
+
+fancy_indexing = img_featured[:, rows, cols]
+fancy_indexing = fancy_indexing.permute(1,0)
+
+
+u,v = feature_to_pixel(rows,cols)
+
+frame_index = torch.full((samples_per_frame,), 0)
+
+# print(u,v)
+print(frame_index.shape)
+# print(img_featured[:,rows[0],cols[0]])
 # print(flat_indices.shape)
 # print(rows.shape, cols.shape)
 # print(rows, cols)
 # print(img_featured.shape)
-print(fancy_indexing)
+
+# print(fancy_indexing)
+
+# print(fancy_indexing.shape)
 
