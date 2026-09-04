292cfe2f94d653051c8409ff4dbaa295c1795803
Shantanu Kadam <shantanukadam3115@gmail.com>
2026-09-04 23:49:42 +0530

    cheat commit.

--- Phase5/preComputePass.py
+++ Phase5/preComputePass.py
@@ -2,44 +2,56 @@ from Phase4.backbone import TruncatedResNetBackbone, feature_to_pixel, normalize
 from Phase3.dataset import CustomDataset
 import torch #type: ignore
 
+
+
 datasetPath = "Phase3\\dataset\\seq-01\\seq-01"
 dataset = CustomDataset(datasetPath)
 backbone = TruncatedResNetBackbone()
 samples_per_frame = 512
 
-img, pose = dataset[0]
+feature_list = []
+pixelcords_list = []
+frameindices_list = []
+pose_list = []
 
-img_normalized = normalizedImage(img)
+for frame_idx in range(len(dataset)):
 
-with torch.no_grad():
-    img_featured = backbone(img_normalized)
-img_featured = img_featured.squeeze(0)
-_, grid_h, grid_w = img_featured.shape
+    img, pose = dataset[frame_idx]
 
+    img_normalized = normalizedImage(img)
 
-flat_indices = torch.randperm(grid_h*grid_w)[:samples_per_frame]
+    with torch.no_grad():
+        img_featured = backbone(img_normalized)
 
-rows, cols = torch.unravel_index(flat_indices, (grid_h, grid_w))
-
-
-
-fancy_indexing = img_featured[:, rows, cols]
-fancy_indexing = fancy_indexing.permute(1,0)
+    img_featured = img_featured.squeeze(0)
+    _, grid_h, grid_w = img_featured.shape
 
 
-u,v = feature_to_pixel(rows,cols)
+    flat_indices = torch.randperm(grid_h*grid_w)[:samples_per_frame]
+    rows, cols = torch.unravel_index(flat_indices, (grid_h, grid_w))
 
-frame_index = torch.full((samples_per_frame,), 0)
 
-# print(u,v)
-print(frame_index.shape)
-# print(img_featured[:,rows[0],cols[0]])
-# print(flat_indices.shape)
-# print(rows.shape, cols.shape)
-# print(rows, cols)
-# print(img_featured.shape)
+    u,v = feature_to_pixel(rows,cols)
+    stacked = torch.stack([u, v], dim=1)
+    pixelcords_list.append(stacked)
 
-# print(fancy_indexing)
+    pose_list.append(pose)
 
-# print(fancy_indexing.shape)
+    fancy_indexing = img_featured[:, rows, cols]
+    fancy_indexing = fancy_indexing.permute(1,0)
+    feature_list.append(fancy_indexing)
 
+
+
+    frame_index = torch.full((samples_per_frame,), frame_idx)
+    frameindices_list.append(frame_index)
+
+all_poses = torch.stack(pose_list, dim=0)
+all_features = torch.stack(feature_list, dim=0)
+all_frameIndices = torch.stack(frameindices_list, dim=0)
+all_pixelcoords = torch.stack(pixelcords_list, dim=0)
+
+print("all_poses shape: ", all_poses.shape)
+print("all_features shape: ", all_features.shape)
+print("all_frameIndices shape: ", all_frameIndices.shape)
+print("all_pixelcoords shape: ", all_pixelcoords.shape)
