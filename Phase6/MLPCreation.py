1aad061e20f364eb23ce1552c86bcdba22162be3
Shantanu Kadam <shantanukadam3115@gmail.com>
2026-09-14 01:40:20 +0530

    Phase6 on going.

--- Phase6/MLPCreation.py
+++ Phase6/MLPCreation.py
@@ -6,9 +6,21 @@ from torch.utils.data import TensorDataset, DataLoader #type: ignore
 data = torch.load("seq_1_buffer.pt")
 
 features = data["features"]
-dataset = TensorDataset(features)
-loader = DataLoader(dataset, batch_size=256, shuffle=True)
+pose = data["poses"]
+pixel_coords = data["pixel_coords"]
 
+feature_dataset = TensorDataset(features)
+feature_loader = DataLoader(feature_dataset, batch_size=2048, shuffle=False)
+
+pose_dataset = TensorDataset(pose)
+pose_loader = DataLoader(pose_dataset, batch_size = 4, shuffle=False )
+
+fx, fy  = 532.57, 531.54
+cx, cy = 320.0,240.0
+k = torch.tensor([[fx,0.0, cx],[0.0,fy,cy],[0.0,0.0,1.0]])
+
+R_cw = pose[0:3, 0:3]
+t_cw = pose[0:3, 3]
 
 class MLP(nn.Module):
     def __init__(self):
@@ -31,27 +43,65 @@ lossFunction = nn.MSELoss()
 optimizer = torch.optim.SGD(linearModel.parameters(),lr=0.001)
 
 result = []
+predicted_world_cords = []
 
 # first_batch_predictions = torch.empty(2000 , 3)
-# print(len(loader))
-# first_batch = next(iter(loader))
+# print(len(feature_loader))
+# first_batch = next(iter(feature_loader))
 # x_batch = first_batch[0]   # unpack the tuple from TensorDataset
-# print(x_batch.shape)       # (256, 64)
-# print(x_batch[0])
+# print(x_batch.shape)       # (250, 64)
+# print(x_batch[0].shape)
+
+# print(len(pose_loader))
+first_batch = next(iter(pose_loader))
+pose_batch = first_batch[0]   # unpack the tuple from TensorDataset
+# print(y_batch.shape)       # (250, 64)
+# print(y_batch[0].shape)
+
+
 
 if __name__ == "__main__":
 
-    for (x_batch) in loader:
+    for (x_batch) in feature_loader:
         for i in range(len(x_batch)):
             
-
             predicted = linearModel(x_batch[i])
             result.append(predicted)
-
-            
+           
         result = torch.cat(result, dim=0)
         break
 
+    feature_chunks = torch.split(result,512)
 
-    print(result.shape)
+    for feature_chunks, pose_batch in zip(feature_chunks,pose_batch):
+        R_cw = pose_batch[0:3, 0:3]
+        t_cw = pose_batch[0:3, 3]
+
+        R_wc = R_cw.mT
+        t_wc = - R_wc @ t_cw.T #[0,0,5]
+        
+        X_cam = feature_chunks@ R_wc.T + t_wc
+        u1,v1, w1 = k @ X_cam.T
+        u1 = u1/w1
+        v1 = v1/w1
+        uv = torch.stack([u1, v1], dim=1)   # (512, 2)
+        predicted_world_cords.append(uv)
+    predicted_world_cords = torch.cat(predicted_world_cords, dim =0)
+
+    pixel_coords_batch1 = pixel_coords[:2048]
+    
+
+    Loss = lossFunction(predicted_world_cords, pixel_coords_batch1)
+
+    print(Loss)
+
+
+            
+
+
+    
+
+    
+
+
 
